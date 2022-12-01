6da2fae155ed889f65bef8ad077b75ec67e16df1
Katsiaryna Lapitskaya <katsiarynalapitskaya@Katsiarynas-MacBook-Pro-2.local>
2022-12-01 15:22:12 +0300

    spaces are deleted, and now User is main actor

--- features/bootcamp.feature
+++ features/bootcamp.feature
@@ -1,15 +1,15 @@
 Feature: Bootcamp E2E
 
 Background: 
-Given I am on the home page
-When I close the promo banner
+Given User is on the home page
+When User closes the promo banner
 
 Scenario: Search bar
-Given I enter Windows in the search bar
-When I click the search button
+Given User enters Windows in the search bar
+When User clicks the search button
 Then At least one item appears
 
 Scenario: Internet shop logo button
-Given I open Today's Best Deals tab
-When I click on the Internet shop logo
-Then I am on the main page
+Given User opens Today's Best Deals tab
+When User clicks on the Internet shop logo
+Then User is on the main page
--- features/step-definitions/steps.js
+++ features/step-definitions/steps.js
@@ -1,10 +1,10 @@
 const { Given, When, Then } = require('@wdio/cucumber-framework');
 
-Given("I am on the home page", async () => {
+Given("User is on the home page", async () => {
     await browser.url("https://www.newegg.com/");
 });
 
-When("I close the promo banner", async () => {
+When("User closes the promo banner", async () => {
     const promo = await $('.modal-body .modal-Website-img');
     try {
         await promo.waitForDisplayed({ timeout: 3000 });
@@ -15,12 +15,12 @@ When("I close the promo banner", async () => {
     }
 })
 
-Given ("I enter {word} in the search bar", async (query) => {
+Given ("User enters {word} in the search bar", async (query) => {
     const input = await $('input[type="search"]');
     await input.setValue(query);
 })
 
-When ("I click the search button", async () => {
+When ("User clicks the search button", async () => {
     const button = await $('.ico-search');
     await button.click();
 })
@@ -33,14 +33,14 @@ Then ("At least one item appears", async () => {
 }) 
 
 
-Given ("I open Today's Best Deals tab", async () => {
+Given ("User opens Today's Best Deals tab", async () => {
     await $('#trendingBanner_720202').click()
 })
 
-When ("I click on the Internet shop logo", async () => {
+When ("User clicks on the Internet shop logo", async () => {
     await $('.header2021-logo-img').click()
 })
 
-Then ("I am on the main page", async () => {
+Then ("User is on the main page", async () => {
     await expect(browser).toHaveUrl("https://www.newegg.com/")
 }) 
